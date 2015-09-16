41463e2a38bf0bcb14173aca50a2b0a0f96ab250
indrianim <indrianimentaruk4@yahoo.com>
2015-09-17 02:54:16 +0900

    Update list.h

--- LinkedList/list.h
+++ LinkedList/list.h
@@ -29,10 +29,12 @@ address alokasi (infotype x);
 void buat_baru(infotype *x);
 void cariid(infotype *x);
 void insertfirst (list *l, address p);
+void insertlast(list *l,address p);
 void insertafter(list *l, address p, address s);
 void deletefirst(list *l,address *p);
 void deletelast(list *l,address *p);
 void deleteafter(list *l, address *p, address s);
+void view (list l);
 address cari(list l, infotype x);
 
 #endif // LIST_H_INCLUDED
