16b789f1d1e1284c0cb1c92b2a5c341a1e982c29
Muhammad Arif Kurniawan <muharifkur@gmail.com>
2015-09-16 20:36:17 +0700

    Update list.cpp

---
+++ LinkedList/list.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+#include <windows.h>
+#include "list.h"
+using namespace std;
+address alokasi (infotype x)
+{
+    address p=(address)malloc(sizeof(Elmlist));
+    info(p)=x;
+    return p;
+}
+void buat_baru(infotype *x)
+{
+    cout<< "=============INSERT DATA KARYAWAN============= "<<endl;
+    infotype k;
+    cout<<"Masukkan ID Karyawan  : ";
+    cin>>k.idKar;
+    cout<<"Masukkan Nama  : ";
+    cin>>k.nama;
+    cout<<"Masukkan Alamat  : ";
+    cin>>k.alm;
+    cout<<"Masukkan Jenis Kelamin  : ";
+    cin>>k.jk;
+    cout<<"Masukkan No. Tlp  : ";
+    cin>>k.notlp;
+    *x=k;
+}
+
+void insertafter(list *l, address p, address s)
+{
+    address Q;
+    Q=(*l).awal;
+    while (info(Q).idKar!=info(s).idKar)
+    {
+        Q=next(Q);
+    }
+    next(p)=NULL;
+    next(p)=next(Q);
+    next(Q)=p;
+
+}
+
+void cariid(infotype *x)
+{
+    cout<< "=============CARI DATA KARYAWAN============= "<<endl;
+    cout<<"Masukkan ID Karyawan yang dicari : ";
+    cin>>(*x).idKar;
+}
+address cari(list l,infotype x)
+{
+    address Q;
+    Q=(l).awal;
+    while(Q!=NULL)
+    {
+        if(info(Q).idKar==x.idKar)
+            return Q;
+        else
+            Q=next(Q);
+    }
+    return NULL;
+}
